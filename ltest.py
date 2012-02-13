b71fd8a03bc55bec9905ae138393e49e8f16700b
BrendanC <brencam@gmail.com>
2012-02-13 13:36:16 -0800

    intial commit

---
+++ ltest.py
@@ -0,0 +1,17 @@
+# lambda demo program
+
+def zz():
+    pass
+
+def test(x,y):    
+    g = lambda x,y:lamda x:(-1/y)*x+y(1)
+    return g
+ 
+
+if "__name__" == "":
+    r = test(1,1)
+    print "Result r: %s" r
+    
+
+           
+           
